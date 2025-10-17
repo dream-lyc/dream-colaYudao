888a3a576011ea170d1fee9101c2e2cde637149a
lyc <lyc01929410@alibaba-inc.com>
2025-10-17 13:40:37 +0800

    初始化-启动web、mybatis成功

---
+++ dream-colaYudao-infra/src/main/java/com/dream/InfraServerApplication.java
@@ -0,0 +1,16 @@
+package com.dream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 项目的启动类
+ */
+@SpringBootApplication
+public class InfraServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InfraServerApplication.class, args);
+    }
+
+}
